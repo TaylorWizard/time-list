8cf317bbcddc1a265d45858ccac22da5006124aa
Weicheng GU <weicheng.gu@hyku.com>
2017-09-18 15:43:58 +0800

    message

--- README.md
+++
@@ -1,3 +0,0 @@
-time-list
-timing List
-[demo](https://github.com/TaylorWizard/time-list/package.json)
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -rf node_modules && npm install && npm run prod
-cp -R ./static ./dist
